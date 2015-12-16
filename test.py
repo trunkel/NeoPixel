fdbdfe7fc12a797dcb1732204d29f051486a43c3
Troy Runkel <trunkel@trunkel-deb7-64>
2015-12-16 08:59:28 -0500

    initial blinkAndFade implementation

--- test.py
+++ test.py
@@ -7,28 +7,46 @@ def getColor(colors):
     colors.updateColor()
     return Color(colors.getRed(), colors.getGreen(), colors.getBlue())
 
-def blinkAndFade(strip, colors, duration_s=60, min_wait_ms=150, max_wait_ms=1000):
+def blinkAndFade(strip, colors, duration_s=60, min_wait_ms=150, max_wait_ms=2000):
     # Set initial pixel state
     state = [random.choice(['On','Off']) for x in range(strip.numPixels())]
     countdown = [random.randint(min_wait_ms,max_wait_ms) for x in range(strip.numPixels())]
 
     for i in range(0, duration_s*1000, min_wait_ms):
         for j in range(strip.numPixels()):
-            if state[j] == 'Fade':
-            else:
-                # Decrement pixel countdown values
-                countdown[j] = max(0, countdown[j] - min_wait_ms);
+            if state[j] == 'On' or state[j] == 'Off':
+                # Decrement pixel countdown value if it's on/off
+                countdown[j] = max(0, countdown[j] - min_wait_ms)
 
                 if countdown[j] == 0:
-                    # Update state of pixels which have timed out
-                    state[j] = random.choice(states)
-                    countdown[j] = random.randint(min_wait_ms, max_wait_ms)
-
-                    # Update pixel color
-                    if state[j] == 'Off':
-                        strip.setPixelColor(j, Color(0, 0, 0))    # off pixel
+                    if state[j] == 'On':
+                        # Pixel is currently 'On'. Start fading to 'Off' state.
+                        state[j] = 'Fade'
                     else:
-                        strip.setPixelColor(j, getColor(colors))  # color pixel
+                        # Pixel is currently 'Off'. Transition to 'On' state.
+                        countdown[j] = random.randint(min_wait_ms, max_wait_ms)
+                        state[j] = 'On'
+
+                        # Set pixel color
+                        strip.setPixelColor(j, getColor(colors))
+
+            if state[j] == 'Fade':
+                # Get current pixel color
+                color = strip.getPixelColor(j)
+
+                # Reduce all color intensities by 16
+                red   = max(0, ((color >> 16) & 0xFF) - 16)
+                green = max(0, ((color >> 8) & 0xFF) - 16)
+                blue  = max(0, (color & 0xFF) - 16)
+
+                # Set new pixel color
+                newColor = Color(red, green, blue)
+                strip.setPixelColor(j, newColor)
+
+                if newColor == 0:
+                    # Pixel has faded to 0. Transition to 'Off' state.
+                    countdown[j] = random.randint(min_wait_ms, max_wait_ms)
+                    state[j] = 'Off'                    
 
         strip.show()
         time.sleep(min_wait_ms/1000.0)                    
@@ -71,7 +89,8 @@ def main():
     strip = Adafruit_NeoPixel(40, 0)
     strip.begin()
 
-    blinker(strip, colors)
+    #blinker(strip, colors)
+    blinkAndFade(strip, colors)
 
     #colorWipe(strip, Color(255, 0, 0))  # Red wipe
     #colorWipe(strip, Color(0, 255, 0))  # Green wipe    
